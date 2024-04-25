c94b7e57fc151081858245c0f944153685be36a0
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-04-25 17:46:16 -0500

    expense status colors

--- components/expense-resume.tsx
+++ components/expense-resume.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { moneyFormatter } from '@/lib/utils'
+import { cn, moneyFormatter } from '@/lib/utils'
 import { PlannedExpenses } from './planned-expenses'
 import { useLocalStorage } from 'usehooks-ts'
 import { useExpenseStore } from '@/store/expense-store'
@@ -20,8 +20,10 @@ export function ExpenseResume() {
 
   const totalSpent = totalExpenses.reduce((acc, value) => acc + value.amount, 0)
 
+  const remainingValue = plannedExpenseValue - totalSpent
+
   return (
-    <nav className="grid grid-cols-2 gap-8 max-w-[1260px] -my-12 mx-auto">
+    <nav className="grid grid-cols-3 gap-8 max-w-[1260px] -my-12 mx-auto">
       <div className="flex flex-col items-start justify-center gap-4 bg-gray-700 rounded-md h-36 p-6">
         <span className="text-white">Total planejado</span>
         <div className="flex items-center gap-2">
@@ -36,8 +38,26 @@ export function ExpenseResume() {
       </div>
       <div className="flex flex-col items-start justify-center gap-4 bg-gray-700 rounded-md h-36 p-6">
         <span className="text-white">Total gasto</span>
-        <p className="font-bold text-3xl text-white">
+        <p
+          className={cn('font-bold text-3xl text-white', {
+            'text-green-500': remainingValue > plannedExpenseValue / 3,
+            'text-yellow-500': remainingValue < plannedExpenseValue / 3,
+            'text-red-500': totalSpent > plannedExpenseValue,
+          })}
+        >
           {moneyFormatter(totalSpent)}
+        </p>
+      </div>
+      <div className="flex flex-col items-start justify-center gap-4 bg-gray-700 rounded-md h-36 p-6">
+        <span className="text-white">Total restante</span>
+        <p
+          className={cn('font-bold text-3xl text-white', {
+            'text-green-500': remainingValue > 0,
+            'text-yellow-500': totalSpent > plannedExpenseValue / 3,
+            'text-red-500': remainingValue < 0,
+          })}
+        >
+          {moneyFormatter(remainingValue)}
         </p>
       </div>
     </nav>
