baeafac71b76c3c8f34092caf77e17e01e13eecb
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-04-25 16:52:44 -0500

    planned expense logic added

--- components/expense-resume.tsx
+++ components/expense-resume.tsx
@@ -1,11 +1,32 @@
+'use client'
+
 import { moneyFormatter } from '@/lib/utils'
+import { PlannedExpenses } from './planned-expenses'
+import { useLocalStorage } from '@uidotdev/usehooks'
 
 export function ExpenseResume() {
+  const [plannedExpenseValue, setPlannedExpenseValue] = useLocalStorage(
+    'plannedExpenseValue',
+    0,
+  )
+
+  const handlePlannedExpenses = (value: number) => {
+    setPlannedExpenseValue(value)
+  }
+
   return (
     <nav className="grid grid-cols-2 gap-8 max-w-[1260px] -my-12 mx-auto">
       <div className="flex flex-col items-start justify-center gap-4 bg-gray-700 rounded-md h-36 p-6">
         <span className="text-white">Total planejado</span>
-        <p className="font-bold text-3xl text-white">{moneyFormatter(12345)}</p>
+        <div className="flex items-center gap-2">
+          <p className="font-bold text-3xl text-white">
+            {moneyFormatter(plannedExpenseValue)}
+          </p>
+          <PlannedExpenses
+            handlePlannedExpenses={handlePlannedExpenses}
+            plannedExpenses={plannedExpenseValue}
+          />
+        </div>
       </div>
       <div className="flex flex-col items-start justify-center gap-4 bg-gray-700 rounded-md h-36 p-6">
         <span className="text-white">Total gasto</span>
---
+++ components/planned-expenses.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { useLocalStorage } from '@uidotdev/usehooks'
+
+import { ChangeEvent, useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog'
+import { Input } from './ui/input'
+import { Pencil } from 'lucide-react'
+import { Button } from './ui/button'
+
+interface Props {
+  plannedExpenses: number
+  handlePlannedExpenses: (value: number) => void
+}
+
+export function PlannedExpenses({
+  plannedExpenses,
+  handlePlannedExpenses,
+}: Props) {
+  const [isOpen, setIsOpen] = useState(false)
+  const [value, setValue] = useState<number>(0)
+
+  const [isFirstAccess, setIsFirstAccess] = useLocalStorage(
+    'firstAccess',
+    'true',
+  )
+
+  const greetingMessage =
+    isFirstAccess && isFirstAccess === 'true'
+      ? 'Olá, seja bem-vindo ao Cash-o-matic'
+      : 'Valor de despesas planejadas'
+
+  const handleChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(Number(event.target.value))
+  }
+
+  const handleSavePlannedExpenses = () => {
+    if (isFirstAccess && isFirstAccess === 'true') {
+      setIsFirstAccess('false')
+    }
+
+    handlePlannedExpenses(value)
+    setIsOpen(false)
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <button>
+          <Pencil className="text-purple-400" />
+        </button>
+      </DialogTrigger>
+      <DialogContent className="bg-gray-800 text-white border-none">
+        <DialogHeader>
+          <DialogTitle>{greetingMessage}</DialogTitle>
+          <DialogDescription className="text-white text-sm font-extralight">
+            Adicione no campo abaixo o quanto você planeja gastar
+          </DialogDescription>
+        </DialogHeader>
+        <Input
+          type="number"
+          defaultValue={plannedExpenses}
+          onChange={handleChangeValue}
+          className="bg-gray-900 text-white h-12 border-none focus-visible:ring-offset-1 focus-visible:ring-2 focus-visible:ring-offset-gray-800  focus-visible:ring-purple-400"
+        />
+        <Button
+          onClick={handleSavePlannedExpenses}
+          className="w-full h-12"
+          type="submit"
+        >
+          Salvar
+        </Button>
+      </DialogContent>
+    </Dialog>
+  )
+}
