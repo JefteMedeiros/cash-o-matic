0da2dfe4063de661be2bdbe1618764369c3f83ac
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-05-02 22:13:12 -0500

    removed expense store

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { ExpenseResume } from '@/components/expense-resume'
 import { ExpenseTable } from '@/components/expense-table'
 import { Logo } from '@/components/logo'
 import { Skeleton } from '@/components/ui/skeleton'
-import { ExpenseProvider } from '@/store/expense-store'
 import { Suspense } from 'react'
 
 function Loading() {
@@ -28,20 +27,18 @@ export default async function Page() {
 
   return (
     <div className="bg-gray-800 min-h-[100dvh] w-full">
-      <ExpenseProvider>
-        <header className="pt-12 pb-24 bg-gray-900">
-          <div className="flex items-center justify-between max-w-[1260px] w-full mx-auto">
-            <Logo />
-            <AddExpense />
-          </div>
-        </header>
-        <ExpenseResume totalExpenses={expenses} />
-        <main className="max-w-[1260px] mx-auto mt-16">
-          <Suspense fallback={<Loading />}>
-            <ExpenseTable />
-          </Suspense>
-        </main>
-      </ExpenseProvider>
+      <header className="pt-12 pb-24 bg-gray-900">
+        <div className="flex items-center justify-between max-w-[1260px] w-full mx-auto">
+          <Logo />
+          <AddExpense />
+        </div>
+      </header>
+      <ExpenseResume totalExpenses={expenses} />
+      <main className="max-w-[1260px] mx-auto mt-16">
+        <Suspense fallback={<Loading />}>
+          <ExpenseTable />
+        </Suspense>
+      </main>
     </div>
   )
 }
