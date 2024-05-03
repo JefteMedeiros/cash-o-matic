620811739b54dc781f32ebfe52fc94da9f7c4930
Jefté Medeiros <jefte.medeiros8@gmail.com>
2024-05-02 19:32:40 -0500

    bun added with drizzle config

---
+++ db/schema.ts
@@ -0,0 +1,16 @@
+import { sql } from 'drizzle-orm'
+import { integer, sqliteTable, text, real } from 'drizzle-orm/sqlite-core'
+
+export const expenses = sqliteTable('expenses', {
+  id: text('id').primaryKey().notNull(),
+  name: text('name').notNull(),
+  value: real('value').notNull(),
+  category: text('category').notNull(),
+  isUnique: integer('boolean', { mode: 'boolean' }).notNull(),
+  createdAt: text('created_at')
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+})
+
+export type InsertExpense = typeof expenses.$inferInsert
+export type SelectExpense = typeof expenses.$inferSelect
